dbefc962aae5cf10851cb1d6c442bd7fb032e06d
Justin Zablocki <justin@quizzle.com>
2016-04-27 14:36:10 -0400

    Add tentative routes and gitignore of .idea

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('for/{slug}', 'CategoryController@showCategory');
+
+Route::get('question/{id}', 'QuestionController@showQuestion');
