0f9e7b7c1721e6a1fc3f24e752e8af01841296ed
Justin Zablocki <justin@quizzle.com>
2016-04-27 15:26:55 -0400

    Add user and admin routes and middleware

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -18,3 +18,31 @@ Route::get('/', function () {
 Route::get('for/{slug}', 'CategoryController@showCategory');
 
 Route::get('question/{id}', 'QuestionController@showQuestion');
+
+/**
+ * Logged in routes
+ */
+Route::group(['namespace' => 'User', 'middleware' => 'auth', 'prefix' => 'user'], function()
+{
+
+});
+
+/**
+ * Admin routes
+ */
+Route::group(['namespace' => 'Admin', 'middleware' => 'admin', 'prefix' => 'admin'], function()
+{
+    // Controllers Within The "App\Http\Controllers\Admin" Namespace
+
+    Route::group(['namespace' => 'User'], function() {
+        // Controllers Within The "App\Http\Controllers\Admin\User" Namespace
+    });
+
+    Route::group(['namespace' => 'Question'], function() {
+
+    });
+
+    Route::group(['namespace' => 'Category'], function() {
+
+    });
+});
