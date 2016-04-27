c961b97d7ec6985fae723c590c3fb59cdc8b0cc3
Justin Zablocki <justin@quizzle.com>
2016-04-27 15:10:24 -0400

    Add migrations and build Questions

---
+++ database/migrations/2016_04_27_185700_create_questions_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateQuestionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('questions', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('user_id')->nullable();
+            $table->string('name');
+            $table->string('slug')->unique();
+            $table->text('question');
+            $table->text('answer');
+            $table->smallInteger('votes');
+            $table->smallInteger('up_votes');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('questions');
+    }
+}
