c86228bba612a3fcd4367155e0ece064ac1dcd9f
Justin Zablocki <justin@quizzle.com>
2016-04-27 20:56:28 -0400

    Add is_admin to User

--- app/User.php
+++ app/User.php
@@ -23,4 +23,8 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    protected $casts = [
+        'is_admin' => 'boolean',
+    ];
 }
