cdee4e89830170506ebc5371ea4b756d27f3f6cb
Justin Zablocki <justin@quizzle.com>
2016-04-27 20:26:39 -0400

    Add category-question model relationships

--- app/Category.php
+++ app/Category.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+    /**
+     * Get the questions
+     */
+    public function comments()
+    {
+        return $this->hasMany('App\Question');
+    }
 }
--- app/Question.php
+++ app/Question.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Question extends Model
 {
-    //
+    /**
+     * Get the category.
+     */
+    public function category()
+    {
+        return $this->belongsTo('App\Category');
+    }
 }
