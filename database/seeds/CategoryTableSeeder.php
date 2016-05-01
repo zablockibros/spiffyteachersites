79ddc158153f361708809be80885e7d387ccda22
Justin Zablocki <justin@quizzle.com>
2016-05-01 13:25:52 -0400

    Add dates to category seeder

--- database/seeds/CategoryTableSeeder.php
+++ database/seeds/CategoryTableSeeder.php
@@ -15,31 +15,43 @@ class CategoryTableSeeder extends Seeder
             'name' => 'Sports Trivia',
             'slug' => 'sports-trivia',
             'description' => '',
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
         ]);
         DB::table('categories')->insert([
             'name' => 'Music Trivia',
             'slug' => 'music-trivia',
             'description' => '',
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
         ]);
         DB::table('categories')->insert([
             'name' => 'Christmas Trivia',
             'slug' => 'christmas-trivia',
             'description' => '',
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
         ]);
         DB::table('categories')->insert([
             'name' => 'Math Trivia',
             'slug' => 'math-trivia',
             'description' => '',
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
         ]);
         DB::table('categories')->insert([
             'name' => 'Movie Trivia',
             'slug' => 'movie-trivia',
             'description' => '',
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
         ]);
         DB::table('categories')->insert([
             'name' => 'History Trivia',
             'slug' => 'history-trivia',
             'description' => '',
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
         ]);
     }
 }
