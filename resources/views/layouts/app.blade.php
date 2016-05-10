f0617cdaa10f32b63eaa8ecee638c3b1d37a6ac2
Justin Zablocki <zablockijj@gmail.com>
2016-05-10 15:49:15 +0000

    Add styles

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,7 +24,7 @@
         <div class="row">
             <div class="top-bar-left">
                 <ul class="dropdown menu" data-dropdown-menu>
-                    <li class="menu-text"><a href="{{ url('/') }}" class="no-pad">TriviaQN</a></li>
+                    <li class="menu-text" style="padding-left:0;"><a href="{{ url('/') }}" class="no-pad">TriviaQN</a></li>
                 </ul>
             </div>
             @if (1 == 2)
@@ -98,6 +98,17 @@
                 $(this).parent().find(".answer").toggleClass("hide");
             });
         });
+    </script>
+
+    <script>
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+        ga('create', 'UA-42272487-28', 'auto');
+        ga('send', 'pageview');
+
     </script>
 </body>
 </html>
