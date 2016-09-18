3509ac3d872932648c95fcf9a7e0ef3767cff6ad
Justin Zablocki <justin@splt.io>
2016-09-18 21:33:36 +0000

    Add Category label column

---
+++ database/migrations/2016_09_18_213227_add_categories_label_column.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddCategoriesLabelColumn extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('categories', function ($table) {
+            $table->string('label')->nullable()->default(null);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('categories', function ($table) {
+            $table->dropField('label');
+        });
+    }
+}
